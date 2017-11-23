8afcc564e11bba24bd45267ee62fb93f9377cbe4
tower1229 <tower1229@qq.com>
2017-11-23 12:59:39 +0800

    remove dist/

--- dist/static/js/5.c9fc02e635e3adecdc51.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{"7FDS":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0,!1,!1)},r=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticStyle:{display:"table",height:"100%",width:"100%"}},[n("div",{staticClass:"g-center"},[n("h1",{staticStyle:{"text-align":"center"}},[e._v("404")])])])}],i={render:l,staticRenderFns:r},s=i,a=n("XAIM"),c=a(null,s,!1,null,null,null);t.default=c.exports}});
-//# sourceMappingURL=5.c9fc02e635e3adecdc51.js.map
