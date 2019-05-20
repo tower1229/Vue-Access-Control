010694719f2c59c09a137214658408a51f91933f
tower1229 <tower1229@foxmail.com>
2019-05-20 09:55:18 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     outputDir: 'docs',
-    baseUrl: process.env.NODE_ENV === 'production'
+    publicPath: process.env.NODE_ENV === 'production'
       ? '/Vue-Access-Control/'
       : '/'
   }
